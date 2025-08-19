21a2a9b43b509be124e4ad75e2eb5001df60e64f
Jess-K92 <jkeating@live.com.au>
2025-08-19 18:58:03 +0800

    created users app and add some config'

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+
+    def __str__(self):
+        return self.username #whenever you get told to print out as a string please return the result to be the username (specified item)
+    
+
+
+
 
 # Create your models here.
